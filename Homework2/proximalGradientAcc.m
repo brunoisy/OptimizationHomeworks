236f9ebe2eaa033601055a9e73957079c029632b
sp.cordonnier <simon-pierre.cordonnier@student.uclouvain.be>
2016-12-08 15:16:06 +0100

    caca

--- Homework2/proximalGradientAcc.m
+++
@@ -1,7 +0,0 @@
-function [ x ] = proximalGradientAcc(A, b, lambda, x0, N, epsilon)
-%PROXIMALGRADIENTACC Summary of this function goes here
-%   Detailed explanation goes here
-
-
-end
-
--- Homework2/testMethods.m
+++
@@ -1,9 +0,0 @@
-A = [1,2;3,4];
-b = [1;2];
-lambda = 0.5;
-x0 = [1;2];
-N = 10;
-epsilon = 0.1;
-
-x = subgradient(A, b, lambda, x0, N, epsilon);
-plot(1:(N+1),x)
