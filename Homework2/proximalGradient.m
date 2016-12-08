3c87a093e901ea1aa629c65c41e26380790f0534
sp.cordonnier <simon-pierre.cordonnier@student.uclouvain.be>
2016-12-08 14:39:49 +0100

    coucou

--- Homework2/proximalGradient.m
+++ Homework2/proximalGradient.m
@@ -2,6 +2,17 @@ function [x] = proximalGradient(A, b, lambda, x0, N, epsilon)
 %PROXIMALGRADIENT Summary of this function goes here
 %   Detailed explanation goes here
 
+n = length(x0);
+L = 2*max(eig(A'*A));
+h = lambda/L;
+
+	function gradf1 = gradf1(x)
+		gradf1 = 2*A'*(A*x-b);
+	end
+
+for k = 1:N
+
+end
 
 end
 
--- Homework2/subgradient.m
+++ Homework2/subgradient.m
@@ -2,6 +2,17 @@ function [ x ] = subgradient(A, b, lambda, x0, N, epsilon)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
+n= length(x0);
+G = 2*norm(A'*(A*x0-b))+sqrt(n)*lambda;
+alpha = 2*epsilon/(G^2);
+
+x = zeros(N+1,1);
+x(1,1) = x0;
+
+for k = 1:N
+	g = 2*A'*(A*x(k)-b)+sign(x(k))*lambda;
+	x(k+1) = x(k)-alpha*g;
+end
 
 end
 
