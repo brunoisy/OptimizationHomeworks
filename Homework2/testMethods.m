ebe423bd0fff599baf8060313f5e2bfb3176fbc3
brunoisy <degomme.bruno@gmail.com>
2016-12-08 14:53:24 +0100

    plop

--- Homework2/testMethods.m
+++ Homework2/testMethods.m
@@ -5,5 +5,12 @@ x0 = [1;2];
 N = 10;
 epsilon = 0.1;
 
+f = @(x)norm(A*x-b)^2+lambda*norm(x,1);
+
 x = smoothedGradient(A, b, lambda, x0, N, epsilon);
-plot(1:(N+1),x)
+
+fx = zeros(1,N+1);
+for i=1:(N+1)
+    fx(i) = f(x(:,i));
+end
+plot(1:(N+1),fx)
