2da03094f777e2a23443b097516cad5062bdb91e
brunoisy <degomme.bruno@gmail.com>
2016-12-22 14:49:28 +0100

    plop

--- Homework2/testMethods.m
+++ Homework2/testMethods.m
@@ -6,19 +6,19 @@ x0 = data.x0;
 
 N = 500;
 epsilon = 0.01;
-xOptimal = 0;% to change!
-fOptimal = 23.7;% to change!
+fOptimal = 23.67;
+NormX0MinusXOptimal = 5.4903;% ||x_0 - x^*||
 
 f = @(x)norm(A*x-b)^2+lambda*norm(x,1);
 
-methods = {@proximalGradientAcc};
-%methods = {@subgradient, @smoothedGradient, @smoothedGradientAcc, @smoothedGradientAcc, @proximalGradient, @proximalGradientAcc, @interiorPoint};
-names = {'Subgradient', 'Smoothed Gradient', 'Accelerated Smoothed Gradient', 'Proximal Gradient', 'Accelerated Proximal Gradient', 'Interior Point'};
+methods = {@smoothedGradient};
+%methods = {@subgradient, @smoothedGradient, @smoothedGradientAcc, @proximalGradient, @proximalGradientAcc, @interiorPoint};
+names = {' Subgradient', ' Smoothed Gradient', ' Accelerated Smoothed Gradient', ' Proximal Gradient', ' Accelerated Proximal Gradient', ' Interior Point'};
 
 
 % G = ***;% ??
 % alpha = 2*epsilon/(G^2);
-% LSmoothed = 2*norm(eigs(A'*A,1))+lambda/epsilon;
+% LSmoothed = 2*normest(A'A)+lambda/epsilon;
 %  
 % boundSubgradient = @(N)norm(x0 - xOptimal)^2/(2*alpha*N) + alpha*G^2/2;
 % boundSmoothedGradient = @(N)epsilon+LSmoothed*norm(x0-xOptimal)^2/N;
@@ -32,13 +32,11 @@ names = {'Subgradient', 'Smoothed Gradient', 'Accelerated Smoothed Gradient', 'P
 for i = 1:length(methods)
     method = methods{i};
     x = method(A, b, lambda, x0, N, epsilon);
-    size(x)
     fx = applyToColumns(f,x);
     figure
-    plot(1:(N+1),fx)
-    %semilogy(1:(N+1),fx-fOptimal) % true convergence.
+    semilogy(1:(N+1),fx-fOptimal) % true convergence.
     hold on
-    title(strcat('Convergence behavior for ', names{i}),'FontSize',16)
+    title(strcat('Convergence behavior for', names{i}),'FontSize',16)
     xlabel('number of iterations k','Fontsize',16);
 %     if (i==1) %subgradient method
 %         ylabel('$\min\limits_{0 \leki \le N} f(x^{(k)})-f(x^*)$','Interpreter','latex','Fontsize',16);
