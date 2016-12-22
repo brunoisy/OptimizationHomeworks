301de30fb97fc58509520acaee6fe1caaa07c77a
brunoisy <degomme.bruno@gmail.com>
2016-12-22 15:30:33 +0100

    tests

--- Homework2/testMethods.m
+++ Homework2/testMethods.m
@@ -6,43 +6,55 @@ x0 = data.x0;
 
 N = 500;
 epsilon = 0.01;
-fOptimal = 23.67;
-NormX0MinusXOptimal = 5.4903;% ||x_0 - x^*||
+fOptimal = 23.67; %25.72?
+normX0MinusXOptimal = 5.4903;% ||x_0 - x^*||
 
 f = @(x)norm(A*x-b)^2+lambda*norm(x,1);
 
-methods = {@smoothedGradient};
-%methods = {@subgradient, @smoothedGradient, @smoothedGradientAcc, @proximalGradient, @proximalGradientAcc, @interiorPoint};
-names = {' Subgradient', ' Smoothed Gradient', ' Accelerated Smoothed Gradient', ' Proximal Gradient', ' Accelerated Proximal Gradient', ' Interior Point'};
 
+methods = {@subgradient, @smoothedGradient, @smoothedGradientAcc, @proximalGradient, @proximalGradientAcc};
+names = {' Subgradient', ' Smoothed Gradient', ' Accelerated Smoothed Gradient', ' Proximal Gradient', ' Accelerated Proximal Gradient'};
 
-% G = ***;% ??
-% alpha = 2*epsilon/(G^2);
-% LSmoothed = 2*normest(A'A)+lambda/epsilon;
-%  
-% boundSubgradient = @(N)norm(x0 - xOptimal)^2/(2*alpha*N) + alpha*G^2/2;
-% boundSmoothedGradient = @(N)epsilon+LSmoothed*norm(x0-xOptimal)^2/N;
-% boundSmoothedGradientAcc = @(N)lambda*mu*N+2*L*norm(x0-xOptimal)^2/(N+1)^2;
-% boundProximalGradient = 
-% boundProximalGradientAcc =
-% boundInteriorPoint = 
-% bounds = {@boundSubgradient, @boundSmoothedGradient, @boundSmoothedGradientAcc, @boundProximalGradient, @boundProximalGradientAcc, @boundInteriorPoint};
+%%% we compute the theoretical upper bounds on convergence rates
+n = length(x0);
+G = 2*norm(A'*(A*x0-b))+sqrt(n)*lambda;
+alpha = 2*epsilon/(G^2);
+L = 2*normest(A'*A);
+mu = epsilon/(lambda*n);
+LSmoothed = L+lambda/mu;
 
-% Plots
-for i = 1:length(methods)
+bndSubgr = @(N)normX0MinusXOptimal^2/(2*alpha*N) + alpha*G^2/2;
+bndSmoothGr = @(N)epsilon+LSmoothed*normX0MinusXOptimal^2/N;
+bndSmoothGrAcc = @(N)epsilon+2*LSmoothed*normX0MinusXOptimal^2/(N+1)^2;
+bndProxGr = @(N)L/(2*N)*normX0MinusXOptimal^2;
+bndProxGrAcc = @(N)L/(2*N)*normX0MinusXOptimal^2;%same?
+% boundInteriorPoint =
+bounds = {applyToColumns(bndSubgr,1:N+1), applyToColumns(bndSmoothGr,1:N+1), applyToColumns(bndSmoothGrAcc,1:N+1), applyToColumns(bndProxGr,1:N+1), applyToColumns(bndProxGrAcc,1:N+1)};
+
+% Plots (every method except interior point)
+for i = 4%1:length(methods) 
     method = methods{i};
     x = method(A, b, lambda, x0, N, epsilon);
     fx = applyToColumns(f,x);
     figure
-    semilogy(1:(N+1),fx-fOptimal) % true convergence.
+    semilogy(1:(N+1),fx-fOptimal) % true convergence
     hold on
+    semilogy(1:(N+1),bounds{i});
     title(strcat('Convergence behavior for', names{i}),'FontSize',16)
     xlabel('number of iterations k','Fontsize',16);
-%     if (i==1) %subgradient method
-%         ylabel('$\min\limits_{0 \leki \le N} f(x^{(k)})-f(x^*)$','Interpreter','latex','Fontsize',16);
-%     else
-%         ylabel('$|f(x_k) - f(x^*)|$','Interpreter','latex','Fontsize',16);
-%     end
-    legend('true convergence', 'theoretical convergence');
+    %     if (i==1) %subgradient method
+    %         ylabel('$\min\limits_{0 \leki \le N} f(x^{(k)})-f(x^*)$','Interpreter','latex','Fontsize',16);
+    %     else
+    %         ylabel('$|f(x_k) - f(x^*)|$','Interpreter','latex','Fontsize',16);
+    %     end
+    legend('true convergence', 'theoretical bound on convergence');
 end
 
+% [~, fx] = interiorPoint(A, b, lambda);
+% figure
+% semilogy(1:length(fx),fx-fOptimal) % true convergence
+% hold on
+% %Plot Bound
+% title('Convergence behavior for interior point','FontSize',16)
+% xlabel('number of iterations k','Fontsize',16);
+% legend('true convergence', 'theoretical bound on convergence');
