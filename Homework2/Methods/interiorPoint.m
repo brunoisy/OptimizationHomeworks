7c00104587a9f27e514026a237dadddab1416f27
sp.cordonnier <simon-pierre.cordonnier@student.uclouvain.be>
2016-12-22 14:27:57 +0100

    debug interior point

--- Homework2/Methods/interiorPoint.m
+++ Homework2/Methods/interiorPoint.m
@@ -1,13 +1,13 @@
-function [ x ] = interiorPoint(A, b, lambda)
+function [ x, z ] = interiorPoint(A, b, lambda)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
 [m,n] = size(A);
 Atilde = [zeros(m,n),          A, zeros(m,n), zeros(m,n), zeros(m,1), zeros(m,1),  -eye(m,m);
 	  zeros(1,n), zeros(1,n), zeros(1,n), zeros(1,n),          1,          0, zeros(1,m);
-	   -eye(n,n),   eye(n,n),   eye(n,n), zeros(n,n), zeros(n,1), zeros(n,1), zeros(n,m);
-	    eye(n,n),   eye(n,n), zeros(n,n),  -eye(n,n), zeros(n,1), zeros(n,1), zeros(n,m)];
-btilde = [b; 1; zeros(n,1); zeros(n,1)];
+	   -eye(n,n),  -eye(n,n),   eye(n,n), zeros(n,n), zeros(n,1), zeros(n,1), zeros(n,m);
+	    eye(n,n),  -eye(n,n), zeros(n,n),  -eye(n,n), zeros(n,1), zeros(n,1), zeros(n,m)];
+btilde = [b; 1/2; zeros(n,1); zeros(n,1)];
 ctilde = [lambda*ones(1,n), zeros(1,n), zeros(1,n), zeros(1,n), 0, 1, zeros(1,m)];
 
 K.f = 2*n;
@@ -15,10 +15,9 @@ K.l = 2*n;
 K.r = m+2;
 [x,y,info]=sedumi_objs(Atilde, btilde, ctilde, K);
 
-z = ctilde*x
+z = ctilde*x;
 
 x = x(n+1:2*n);
-
-fx = (norm(A*x-b))^2 + lambda*norm(x,1)  
+ 
 end
 
